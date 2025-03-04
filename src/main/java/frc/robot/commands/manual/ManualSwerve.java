db19c8f4cf8afa73449126c31c61182eb68ac5b5
Isaac <barkerisaacd@gmail.com>
2025-03-03 19:21:51 -0500

    Add limits

--- src/main/java/frc/robot/commands/manual/ManualSwerve.java
+++ src/main/java/frc/robot/commands/manual/ManualSwerve.java
@@ -4,11 +4,14 @@
 
 package frc.robot.commands.manual;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.control.AbstractControl;
 import frc.robot.dashboard.DashboardUI;
 import frc.robot.utils.DriveCommandData;
+import org.littletonrobotics.junction.Logger;
 
 /** An example command that uses an example subsystem. */
 public class ManualSwerve extends Command {
@@ -27,10 +30,43 @@ public class ManualSwerve extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    double elevatorHeightFromTop =
+        Constants.Elevator.MAX_HEIGHT - RobotContainer.elevator.getCurrentHeight();
+    double maxAcceleration =
+        Constants.Elevator.DRIVETRAIN_ACCELERATION_PER_ELEVATOR_METER_FROM_TOP
+                * elevatorHeightFromTop
+            + Constants.Elevator.DRIVETRAIN_ACCELERATION_WHEN_AT_TOP;
+
     AbstractControl controls = DashboardUI.Overview.getControl();
 
     DriveCommandData driveCommandData = controls.getDriveCommandData();
+
+    ChassisSpeeds currentSpeeds = RobotContainer.drivetrain.getChassisSpeeds();
+    ChassisSpeeds commandedSpeeds =
+        ChassisSpeeds.fromFieldRelativeSpeeds(
+            driveCommandData.xSpeed,
+            driveCommandData.ySpeed,
+            driveCommandData.rot,
+            RobotContainer.poseTracker.getEstimatedPosition().getRotation());
+    ChassisSpeeds diffSpeeds = commandedSpeeds.minus(currentSpeeds);
+    double translationalAcceleration =
+        Math.sqrt(
+            diffSpeeds.vxMetersPerSecond * diffSpeeds.vxMetersPerSecond
+                + diffSpeeds.vyMetersPerSecond * diffSpeeds.vyMetersPerSecond); // m/s^2
+    if (translationalAcceleration > maxAcceleration) {
+      double percentOver = translationalAcceleration / maxAcceleration;
+      driveCommandData.xSpeed -= currentSpeeds.vxMetersPerSecond;
+      driveCommandData.ySpeed -= currentSpeeds.vyMetersPerSecond;
+      driveCommandData.xSpeed *= 1 / percentOver;
+      driveCommandData.ySpeed *= 1 / percentOver;
+      driveCommandData.xSpeed += currentSpeeds.vxMetersPerSecond;
+      driveCommandData.ySpeed += currentSpeeds.vyMetersPerSecond;
+    }
+
     RobotContainer.drivetrain.drive(driveCommandData);
+
+    Logger.recordOutput("currentSpeeds", currentSpeeds);
+    Logger.recordOutput("driveCommandDataSpeeds", commandedSpeeds);
   }
 
   // Called once the command ends or is interrupted.
