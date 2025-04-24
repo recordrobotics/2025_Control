ab333f0c00d8bbd35ba961a53b30054e923dcffe
Isaac <barkerisaacd@gmail.com>
2025-04-24 14:41:36 -0400

    refactor climber tab switching

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -62,6 +62,7 @@ import frc.robot.utils.HumanPlayerSimulation;
 import frc.robot.utils.PoweredSubsystem;
 import frc.robot.utils.RepeatConditionallyCommand;
 import frc.robot.utils.ShuffleboardPublisher;
+import frc.robot.utils.libraries.Elastic;
 import java.util.function.BooleanSupplier;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.photonvision.simulation.VisionSystemSim;
@@ -190,9 +191,10 @@ public class RobotContainer {
     new Trigger(() -> DashboardUI.Overview.getControl().getClimb())
         .onTrue(
             Commands.either(
-                new ClimbMove(ClimberState.Climb),
-                new ClimbMove(ClimberState.Extend),
-                () -> climber.getCurrentState() == ClimberState.Extend));
+                    new ClimbMove(ClimberState.Climb),
+                    new ClimbMove(ClimberState.Extend),
+                    () -> climber.getCurrentState() == ClimberState.Extend)
+                .alongWith(new InstantCommand(() -> Elastic.selectTab("Climb"))));
 
     // Reset pose trigger
     new Trigger(() -> DashboardUI.Overview.getControl().getPoseReset())
@@ -203,28 +205,6 @@ public class RobotContainer {
         .onTrue(new InstantCommand(poseTracker::resetStartingPose).ignoringDisable(true));
     new Trigger(() -> DashboardUI.Autonomous.getLimelightRotation())
         .onTrue(new InstantCommand(poseTracker::resetToLimelight).ignoringDisable(true));
-
-    // Climb mode trigger
-    // new Trigger(() -> DashboardUI.Overview.getControl().getClimbMode())
-    //     .onTrue(
-    //         new InstantCommand(
-    //                 () -> {
-    //                   if (!inClimbMode) {
-    //                     inClimbMode = true;
-    //                     Elastic.selectTab("Climb");
-    //                   } else {
-    //                     inClimbMode = false;
-    //                     DashboardUI.Overview.switchTo();
-    //                   }
-    //                 })
-    //             .andThen(
-    //                 new DeferredCommand(
-    //                     () ->
-    //                         new ClimbMove(
-    //                             climber.getCurrentState() == ClimberState.Extend
-    //                                 ? ClimberState.Park
-    //                                 : ClimberState.Extend),
-    //                     Set.of(climber))));
 
     BooleanSupplier elevatorLock =
         () -> {
