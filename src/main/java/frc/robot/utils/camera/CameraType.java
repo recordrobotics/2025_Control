d3c1a7ad6dc8edd13fe96ee659162a7a150e4bb2
Compdog Inc. <compdog.inc@gmail.com>
2025-10-27 18:43:46 -0400

    Increase txty confidence

--- src/main/java/frc/robot/utils/camera/CameraType.java
+++ src/main/java/frc/robot/utils/camera/CameraType.java
@@ -1,11 +1,11 @@
 package frc.robot.utils.camera;
 
 public enum CameraType {
-    LIMELIGHT_2(1280, 960, 2, 74.36, 13, 0.2, 0.0005, 35, 5, 0.55, 1.4175, 0.65),
+    LIMELIGHT_2(1280, 960, 2, 74.36, 13, 0.2, 0.0005, 35, 5, 0.55, 1.4175, 0.35),
     LIMELIGHT_3G(
             1280, 960, 1, 100, 8, 0.2, 0.0005, 35, 5, 0.55, 1.11574,
-            0.65), // TODO: what is the actual fov of new lens we replaced
-    SVPRO_GLOBAL_SHUTTER(1920, 1200, 1, 83, 23, 0.2, 0.0005, 35, 5, 0.55, 1.3, 0.65);
+            0.35), // TODO: what is the actual fov of new lens we replaced
+    SVPRO_GLOBAL_SHUTTER(1920, 1200, 1, 83, 23, 0.2, 0.0005, 35, 5, 0.55, 1.3, 0.35);
 
     final int width;
     final int height;
